1e6c001c1c37877b28dcdc5845ef14582742ab7c
vinayakgrover <vinayakgrover@Vinayaks-MBP.lan>
2024-10-03 18:45:14 -0400

    test

--- customer_banking.py
+++ customer_banking.py
@@ -1,5 +1,6 @@
 # Import the create_cd_account and create_savings_account functions
-# ADD YOUR CODE HERE
+from savings_account import create_savings_account
+from cd_account import create_cd_account
 
 # Define the main function
 def main():
@@ -8,22 +9,27 @@ def main():
     It displays the interest earned on the savings and CD accounts and updates the balances.
     """
     # Prompt the user to set the savings balance, interest rate, and months for the savings account.
-    # ADD YOUR CODE HERE
+    savings_balance = float(input("what is your balance"))
+    savings_interest = float(input("whats the IR?"))
+    savings_maturity = int(input("How many months?"))
 
     # Call the create_savings_account function and pass the variables from the user.
     updated_savings_balance, interest_earned = create_savings_account(savings_balance, savings_interest, savings_maturity)
 
     # Print out the interest earned and updated savings account balance with interest earned for the given months.
-    # ADD YOUR CODE HERE
+    print(f"You have earned {interest_earned} in {savings_maturity} months, and your new account balance is {updated_savings_balance}")
 
     # Prompt the user to set the CD balance, interest rate, and months for the CD account.
-    # ADD YOUR CODE HERE
+    cd_balance = float(input("what is your balance"))
+    cd_interest = float(input("whats the IR?"))
+    cd_maturity = int(input("How many months?"))
 
     # Call the create_cd_account function and pass the variables from the user.
     updated_cd_balance, interest_earned = create_cd_account(cd_balance, cd_interest, cd_maturity)
 
     # Print out the interest earned and updated CD account balance with interest earned for the given months.
-    # ADD YOUR CODE HERE
+    print(f"You have earned {interest_earned} in {cd_maturity} months, and your new account balance is {updated_cd_balance}")
 
 if __name__ == "__main__":
     # Call the main function.
+    main()
